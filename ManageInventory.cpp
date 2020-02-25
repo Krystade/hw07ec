e82321c3449fb964b1d444a3505d5be8e9a6d5c4
Krys <21113012+Krystade@users.noreply.github.com>
2020-02-24 23:32:10 -0800

    Update ManageInventory.cpp

--- ManageInventory.cpp
+++ ManageInventory.cpp
@@ -4,28 +4,47 @@
  *  Created on: Feb 24, 2020
  *      Author: Jack
  */
-#include "hw07ec.h"
 #include "ManageInventory.h"
 
+
+//Default ManageInventory constructor
+ManageInventory::ManageInventory(){
+	size = MAX_SIZE;
+	count = 0;
+	p_pInventoryItems = new Item*[size];
+}
+//Overloaded ManageInventory constructor
+ManageInventory::ManageInventory(int size){
+	this->size = size;
+	count = 0;
+	p_pInventoryItems = new Item*[size];
+}
+//ManageInventory copy constructor
+ManageInventory::ManageInventory(const ManageInventory& source){
+	size = source.size;
+	count = 0;
+	p_pInventoryItems = new Item*[size];
+	//Loop through each value in the original's array in order to make a deep copy
+	for(int i = 0; i < source.count; i++){
+		p_pInventoryItems[i] = new Item();
+		p_pInventoryItems[i]->name = source.p_pInventoryItems[i]->name;
+		p_pInventoryItems[i]->quantity = source.p_pInventoryItems[i]->quantity;
+		p_pInventoryItems[i]->cost = source.p_pInventoryItems[i]->cost;
+		count++;
+	}
+}
+//ManageInventory destructor
 ManageInventory::~ManageInventory(){
 	for(int i = 0; i < size; i++){
 		delete p_pInventoryItems[i];
 	}
 	delete[] p_pInventoryItems;
 }
+//Function to add a new Item into the p_pInventoryItems array
 void ManageInventory::addItem(string name, int quantity, float cost){
+	p_pInventoryItems[count] = new Item();
+	p_pInventoryItems[count]->name = name;
+	p_pInventoryItems[count]->quantity = quantity;
+	p_pInventoryItems[count]->cost = cost;
 	count++;
-	if(count < size){
-		p_pInventoryItems[count] = new Item();
-	}
-}
-ManageInventory::ManageInventory(){
-	size = MAX_SIZE;
-	count = 0;
-	p_pInventoryItems = new Item*[size];
-}
-ManageInventory::ManageInventory(int size){
-	count = 0;
-	this->size = size;
-	p_pInventoryItems = new Item*[size];
 }
